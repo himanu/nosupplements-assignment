52190319a7f7032e696899f1bd4fb719f486448b
Himanshu <hy287719@gmail.com>
2021-12-11 19:19:35 +0530

    Changed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./index.css";
+import { useEffect, useState } from "react";
+import User from "./User";
+export default function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      let data = await response.json();
+      setUsers(data);
+    }
+    fetchData();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users.map((user) => {
+        return (
+          <User
+            user={user}
+            key={user.id}
+            handleDelete={(userId) => {
+              setUsers((oldUsers) => {
+                return oldUsers.filter((user1) => user1.id !== userId);
+              });
+            }}
+            editUser={(name, email, phone, website) => {
+              setUsers((oldUsers) => {
+                return oldUsers.map((user1) => {
+                  if (user1.id !== user.id) {
+                    return user1;
+                  } else {
+                    return {
+                      ...user1,
+                      name,
+                      email,
+                      phone,
+                      website
+                    };
+                  }
+                });
+              });
+            }}
+          />
+        );
+      })}
     </div>
   );
 }
-
-export default App;
